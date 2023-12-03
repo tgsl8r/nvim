799d077148ffb0529d8a9746d88499f97a30ba68
tslater <toby@tgs.onl>
2023-12-03 15:15:51 +0000

    Add which-key config and name terminal submenu

---
+++ .config/nvim/lua/plugins/which-key.lua
@@ -0,0 +1,13 @@
+return {
+  -- Modify which-key keys
+  {
+    "folke/which-key.nvim",
+    opts = function()
+      require("which-key").register({
+        ["<leader>t"] = {
+          name = "+terminal",
+        },
+      })
+    end,
+  },
+}
