d41e0324ddde629ac6325683d67929dab6d7d35f
Toby Slater <toby@tgs.onl>
2025-11-02 01:15:52 +0000

    feat(core): Config and set colorscheme

---
+++ lua/usr/core/color.lua
@@ -0,0 +1,15 @@
+-- Colorscheme options
+-- vim.g.gruvbox_material_background = "soft"
+vim.g.gruvbox_flat_style = "dark"
+vim.g.gruvbox_italic_comments = false
+vim.g.gruvbox_italic_keywordss = false
+
+-- Set colourscheme
+local colorscheme = "gruvbox-flat"
+local colorscheme_status, _ = pcall(vim.cmd.colorscheme, colorscheme)
+if not colorscheme_status then
+	print("Colorscheme " .. colorscheme .. " not found!")
+	return
+end
+
+
