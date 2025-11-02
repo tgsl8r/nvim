dea11d14dcd9f2d7069e7810dd78b8d9fbc45c2d
Toby Slater <toby@tgs.onl>
2025-11-02 01:11:18 +0000

    feat(/init): New root init.lua

---
+++ init.lua
@@ -0,0 +1,12 @@
+-- ░▀█▀░█▀█░▀█▀░▀█▀░░░░█░░░█░█░█▀█░
+-- ░░█░░█░█░░█░░░█░░░░░█░░░█░█░█▀█░
+-- ░▀▀▀░▀░▀░▀▀▀░░▀░░▀░░▀▀▀░▀▀▀░▀░▀░
+
+-- Install plugins --
+require("usr.install")
+
+-- Plugin config
+require("usr.plugins")
+
+-- Core --
+require("usr.core")
