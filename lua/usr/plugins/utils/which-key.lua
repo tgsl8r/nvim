cfe206b2544c174af515d4af411a9de4c3506e69
Toby Slater <toby@tgs.onl>
2025-11-02 01:15:16 +0000

    feat(plugins):  Add whichkey

---
+++ lua/usr/plugins/utils/which-key.lua
@@ -0,0 +1,33 @@
+local whichkey_status, whichkey = pcall(require, "which-key")
+if not whichkey_status then
+	print("Couldn't load which-key")
+	return
+end
+
+vim.o.timeout = true
+vim.o.timeoutlen = 300
+
+whichkey.setup({
+	preset = "helix",
+	defaults = {},
+	spec = {
+		mode = { "n", "v" },
+		{ "[", group = "prev" },
+		{ "]", group = "next" },
+		{ "g", group = "goto" },
+		{ "<leader>f", group = "files" },
+		{ "<leader>w", group = "windows" },
+		{ "<leader>b", group = "buffers" },
+		{ "<leader>s", group = "search/show" },
+		{ "<leader>c", group = "code" },
+		{ "<leader>g", group = "git" },
+		{ "<leader>gh", group = "hunk" },
+		{ "<leader>h", group = "history" },
+		{ "<leader>d", group = "debug" },
+		{ "<leader>dp", group = "profiling" },
+		{ "<leader>t", group = "neotest" },
+		{ "<leader>u", group = "utils" },
+		{ "<leader>ca", vim.lsp.buf.code_action, desc = "Code Action" },
+		{ "<leader>cr", vim.lsp.buf.rename, desc = "Rename Symbol" },
+	},
+})
