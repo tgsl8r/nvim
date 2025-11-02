2cfa9464050d7fbbd7a9a47aac97b4868a2a8c2a
Toby Slater <toby@tgs.onl>
2025-11-02 01:14:39 +0000

    feat(install): Add install.lua  installing plugins

---
+++ lua/usr/install.lua
@@ -0,0 +1,13 @@
+vim.pack.add({
+  -- UI
+	{ src = "git@github.com:eddyekofo94/gruvbox-flat.nvim" },
+	{ src = "git@github.com:folke/snacks.nvim" },
+	{ src = "git@github.com:folke/which-key.nvim" },
+  -- Treesitter
+	{ src = "git@github.com:nvim-treesitter/nvim-treesitter.git" },
+	{ src = "git@github.com:nvim-treesitter/nvim-treesitter-textobjects.git" },
+  -- LSP
+	{ src = "git@github.com:mason-org/mason.nvim.git" },
+  -- Code
+	{ src = "git@github.com:lewis6991/gitsigns.nvim.git" },
+})
