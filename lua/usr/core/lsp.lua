372147ba2cd53d15e805a9ae51e5d9326ec1f44f
tslater <toby@tgs.onl>
2026-01-02 22:49:13 +0000

    feat(lsp.lua): LSP keybinds

--- lua/usr/core/lsp.lua
+++ lua/usr/core/lsp.lua
@@ -32,3 +32,30 @@ require('mason-lspconfig').setup({
   automatic_installation = true,
 })
 
+------------------------------------------------------------
+-- LSP keymaps
+------------------------------------------------------------
+
+local function on_attach(client, bufnr)
+  local opts = { noremap = true, silent = true, buffer = bufnr }
+
+  vim.keymap.set('i', '<C-k>', vim.lsp.buf.signature_help, opts)
+
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+  vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, opts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+
+  vim.keymap.set('n', '<leader>cr', vim.lsp.buf.rename, opts)
+  vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
+  vim.keymap.set('n', '<leader>cf', function()
+    vim.lsp.buf.format({ async = true })
+  end, opts)
+  vim.keymap.set('n', '<leader>cd', vim.diagnostic.open_float, opts)
+  vim.keymap.set('n', '<leader>cl', vim.diagnostic.setloclist, opts)
+
+  vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+  vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+end
+
