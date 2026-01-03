b1b541ec782c3e508a4b2a5820c98b592586c757
tslater <toby@tgs.onl>
2026-01-03 21:52:02 +0000

    feat(lsp): Add lua_ls lsp

---
+++ lsp/lua_ls.lua
@@ -0,0 +1,14 @@
+-- lua_ls.lua
+
+return {
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = {
+                    "vim",
+                },
+            },
+        },
+    },
+}
+
