c78df5c022bab5e3772fd5df0cd7ec3671b5fe56
tslater <toby@tgs.onl>
2026-01-09 11:02:42 +0000

    fix(keymaps): Make type def bing more mnemonic

--- lua/usr/core/keymaps.lua
+++ lua/usr/core/keymaps.lua
@@ -130,7 +130,7 @@ local bindings = {
     { "gD",          function() Snacks.picker.lsp_declarations() end, desc = "Goto Declaration" },
     { "gr",          function() Snacks.picker.lsp_references() end, nowait = true, desc = "References" },
     { "gI",          function() Snacks.picker.lsp_implementations() end, desc = "Goto Implementation" },
-    { "gy",          function() Snacks.picker.lsp_type_definitions() end, desc = "Goto T[y]pe Definition" },
+    { "gt",          function() Snacks.picker.lsp_type_definitions() end, desc = "Goto T[y]pe Definition" },
     { "gai",         function() Snacks.picker.lsp_incoming_calls() end, desc = "C[a]lls Incoming" },
     { "gao",         function() Snacks.picker.lsp_outgoing_calls() end, desc = "C[a]lls Outgoing" },
     { "<leader>ss",  function() Snacks.picker.lsp_symbols() end, desc = "LSP Symbols" },
