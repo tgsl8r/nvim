1849e136e08dcf2ec8bc71bfafd7e1c85220d93b
tslater <toby@tgs.onl>
2026-01-09 11:08:34 +0000

    fix(lsp): Remove keybinds from lsp.lua

--- lua/usr/plugins/code/lsp.lua
+++ lua/usr/plugins/code/lsp.lua
@@ -33,31 +33,6 @@ require('mason-lspconfig').setup({
 })
 
 ------------------------------------------------------------
--- LSP keymaps
-------------------------------------------------------------
-
-local function on_attach(client, bufnr)
-  local opts = { noremap = true, silent = true, buffer = bufnr }
-
-  vim.keymap.set('i', '<C-k>', vim.lsp.buf.signature_help, opts)
-
-  vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-  vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, opts)
-  vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-
-  vim.keymap.set('n', '<leader>cr', vim.lsp.buf.rename, opts)
-  vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
-  vim.keymap.set('n', '<leader>cf', function()
-    vim.lsp.buf.format({ async = true })
-  end, opts)
-  vim.keymap.set('n', '<leader>cd', vim.diagnostic.open_float, opts)
-  vim.keymap.set('n', '<leader>cl', vim.diagnostic.setloclist, opts)
-
-end
-
-------------------------------------------------------------
 -- Load config and enable servers
 ------------------------------------------------------------
 
