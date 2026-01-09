8bbbd32223e80e2f949a9109978e390c07f3d948
tslater <toby@tgs.onl>
2026-01-09 11:40:28 +0000

    feat(plugins): Add initial dap.lua

---
+++ lua/usr/plugins/code/dap.lua
@@ -0,0 +1,36 @@
+-- lsp.lua
+-- LSP configuration
+
+------------------------------------------------------------
+-- Servers
+------------------------------------------------------------
+
+local servers = {
+    "clangd",
+    "basedpyright",
+    "lua_ls",
+    "ruff",
+}
+
+------------------------------------------------------------
+-- Plugins
+------------------------------------------------------------
+
+vim.pack.add({
+    { src = "git@github.com:neovim/nvim-lspconfig" },
+    { src = "git@github.com:williamboman/mason.nvim" },
+    { src = "git@github.com:williamboman/mason-lspconfig.nvim" },
+    { src = "git@github.com:WhoIsSethDaniel/mason-tool-installer.nvim" },
+})
+
+------------------------------------------------------------
+-- Mason
+------------------------------------------------------------
+
+require('mason').setup()
+
+require('mason-lspconfig').setup()
+
+require('mason-tool-installer').setup({
+    ensure_installed = servers
+})
