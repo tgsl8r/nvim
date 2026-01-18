6415717fd6b2bf33fb54f4f2ec19cd4f455fd022
tslater <toby@tgs.onl>
2026-01-18 20:13:28 +0000

    feat(plugins): Add initial neotest config

---
+++ lua/usr/plugins/code/tests.lua
@@ -0,0 +1,27 @@
+-- tests.lua
+-- Config for neotest and adapters
+
+------------------------------------------------------------
+-- Plugins
+------------------------------------------------------------
+
+vim.pack.add({
+    { src = "git@github.com:nvim-neotest/neotest" },
+    { src = "git@github.com:orjangj/neotest-ctest" },
+    { src = "git@github.com:nvim-neotest/neotest-python" },
+})
+
+
+------------------------------------------------------------
+-- Config
+------------------------------------------------------------
+
+local neotest = require("neotest")
+
+neotest.setup({
+  adapters = {
+    require("neotest-ctest")({
+      build_dir = "build",
+    }),
+  },
+})
