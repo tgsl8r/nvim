84977aab4641b550d284e81f6d0987d0a6769857
tslater <toby@tgs.onl>
2026-01-20 18:11:32 +0000

    feat(autocmds): Add buffer reload autocmd

---
+++ lua/usr/core/autocmds.lua
@@ -0,0 +1,17 @@
+-- autocmds.lua
+-- Setup vim autocmds
+
+local function augroup(name)
+  return vim.api.nvim_create_augroup("usr_" .. name, { clear = true })
+end
+
+-- Check if we need to reload the file when it changed
+vim.api.nvim_create_autocmd({ "FocusGained", "TermClose", "TermLeave" }, {
+  group = augroup("checktime"),
+  callback = function()
+    if vim.o.buftype ~= "nofile" then
+      vim.cmd("checktime")
+    end
+  end,
+})
+
