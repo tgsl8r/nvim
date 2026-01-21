b218893bec041e93a4912b6fe70efeae9374a048
tslater <toby@tgs.onl>
2026-01-21 10:06:34 +0000

    feat(autocmds): Add split resize

--- lua/usr/core/autocmds.lua
+++ lua/usr/core/autocmds.lua
@@ -22,3 +22,13 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     (vim.hl or vim.highlight).on_yank()
   end,
 })
+
+-- resize splits if window got resized
+vim.api.nvim_create_autocmd({ "VimResized" }, {
+  group = augroup("resize_splits"),
+  callback = function()
+    local current_tab = vim.fn.tabpagenr()
+    vim.cmd("tabdo wincmd =")
+    vim.cmd("tabnext " .. current_tab)
+  end,
+})
