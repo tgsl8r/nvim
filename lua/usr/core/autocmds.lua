56ba3fc7dcb37dbcf05a3936dfd35cfa3fd79394
tslater <toby@tgs.onl>
2026-01-21 10:08:46 +0000

    feat(autocmds): Add auto wrap and spellcheck in filetypes

--- lua/usr/core/autocmds.lua
+++ lua/usr/core/autocmds.lua
@@ -94,3 +94,13 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.bo[event.buf].buflisted = false
   end,
 })
+
+-- wrap and check for spell in text filetypes
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("wrap_spell"),
+  pattern = { "text", "plaintex", "typst", "gitcommit", "markdown" },
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.spell = true
+  end,
+})
