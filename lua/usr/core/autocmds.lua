5863eec51ddb1ee998c95b230f75b7937c25ff19
tslater <toby@tgs.onl>
2026-01-21 10:08:07 +0000

    feat(autocmds): Add close inline man pages

--- lua/usr/core/autocmds.lua
+++ lua/usr/core/autocmds.lua
@@ -85,3 +85,12 @@ vim.api.nvim_create_autocmd("FileType", {
     end)
   end,
 })
+
+-- make it easier to close man-files when opened inline
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("man_unlisted"),
+  pattern = { "man" },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+  end,
+})
