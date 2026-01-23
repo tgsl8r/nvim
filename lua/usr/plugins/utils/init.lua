f0dad563421f0526a27e62f8b06ea453520afa9e
tslater <toby@tgs.onl>
2026-01-23 15:46:43 +0000

    feat(plugins): Add bufferline and basic conf

--- lua/usr/plugins/utils/init.lua
+++ lua/usr/plugins/utils/init.lua
@@ -1,3 +1,4 @@
 require("usr.plugins.utils.color")
 require("usr.plugins.utils.which-key")
 require("usr.plugins.utils.snacks")
+require("usr.plugins.utils.ui")
---
+++ lua/usr/plugins/utils/ui.lua
@@ -0,0 +1,23 @@
+local bufferline_status, bufferline = pcall(require, "bufferline")
+if not bufferline_status then
+	print("Couldn't load bufferline")
+	return
+end
+
+bufferline.setup({
+    options = {
+        diagnostics = "nvim_lsp",
+        always_show_bufferline = false,
+        offsets = {
+        {
+          filetype = "neo-tree",
+          text = "Neo-tree",
+          highlight = "Directory",
+          text_align = "left",
+        },
+        {
+          filetype = "snacks_layout_box",
+        },
+        },
+    }
+})
