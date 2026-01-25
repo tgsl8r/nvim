775fb98438672c8f9ee7783497dab7e0a9d3167d
tslater <toby@tgs.onl>
2026-01-25 18:41:21 +0000

    feat(color): Configure Bamboo

--- lua/usr/plugins/utils/color.lua
+++ lua/usr/plugins/utils/color.lua
@@ -11,4 +11,38 @@ if not bamboo_status then
 	return
 end
 
+bamboo.setup({
+    -- Main options --
+    style = 'vulgaris', -- Choose between 'vulgaris' (regular), 'multiplex' (greener), and 'light'
+    toggle_style_key = nil, -- Keybind to toggle theme style. Leave it nil to disable it, or set it to a string, e.g. "<leader>ts"
+    toggle_style_list = { 'vulgaris', 'multiplex', 'light' }, -- List of styles to toggle between
+    transparent = false, -- Show/hide background
+    dim_inactive = true, -- Dim inactive windows/buffers
+    term_colors = true, -- Change terminal color as per the selected theme style
+    ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
+    cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
 
+    -- Change code style ---
+    -- Options are anything that can be passed to the `vim.api.nvim_set_hl` table
+    -- You can also configure styles with a string, e.g. keywords = 'italic,bold'
+    code_style = {
+        comments = { italic = false },
+        conditionals = { italic = false },
+        keywords = {},
+        functions = {},
+        namespaces = { italic = false },
+        parameters = { italic = false },
+        strings = {},
+        variables = {},
+    },
+    -- Custom Highlights --
+    colors = {}, -- Override default colors
+    highlights = {}, -- Override highlight groups
+
+    -- Plugins Config --
+    diagnostics = {
+        darker = false, -- darker colors for diagnostic
+        undercurl = true, -- use undercurl instead of underline for diagnostics
+        background = true, -- use background color for virtual text
+    },
+})
