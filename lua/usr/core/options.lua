75d4fc3204a64cf04413a3b10e84db9faf0b8036
tslater <toby@tgs.onl>
2026-01-26 21:36:11 +0000

    feat(options): Set default softtabstop

--- lua/usr/core/options.lua
+++ lua/usr/core/options.lua
@@ -33,6 +33,7 @@ vim.opt.smartindent = true
 vim.opt.shiftwidth = 4 -- the number of spaces inserted for each indentation
 vim.opt.expandtab = true
 vim.opt.tabstop = 4 -- insert 4 spaces for a tab
+vim.opt.softtabstop = 4 -- in-editor tab size
 
 -- Line wrapping
 vim.opt.wrap = false
