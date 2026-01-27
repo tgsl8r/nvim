19ad873e783cd891a784882a9ee9ece494cbf749
tslater <toby@tgs.onl>
2026-01-27 13:41:25 +0000

    style(plugins): Formatting plugins.lua

--- lua/usr/core/plugins.lua
+++ lua/usr/core/plugins.lua
@@ -2,27 +2,27 @@
 -- Install and import the config of plugins
 
 vim.pack.add({
-  -- deps
+    -- deps
     { src = "git@github.com:nvim-lua/plenary.nvim.git" },
     { src = "git@github.com:nvim-tree/nvim-web-devicons" },
-  -- UI
-	{ src = "git@github.com:folke/snacks.nvim" },
-	{ src = "git@github.com:folke/which-key.nvim" },
-	{ src = "git@github.com:folke/trouble.nvim" },
-	{ src = "git@github.com:folke/todo-comments.nvim" },
-	{ src = "git@github.com:akinsho/bufferline.nvim" },
-  -- Colourschemes
-	{ src = "git@github.com:eddyekofo94/gruvbox-flat.nvim" },
-	{ src = "git@github.com:ribru17/bamboo.nvim" },
-  -- Treesitter
-	{ src = "git@github.com:nvim-treesitter/nvim-treesitter.git" },
-	{ src = "git@github.com:nvim-treesitter/nvim-treesitter-textobjects.git" },
-	{ src = "git@github.com:nvim-treesitter/nvim-treesitter-context.git" },
-  -- Code
-	{ src = "git@github.com:lewis6991/gitsigns.nvim.git" },
-  -- Editing
-	{ src = "git@github.com:nvim-mini/mini.ai" },
-	{ src = "git@github.com:nvim-mini/mini.surround" },
+    -- UI
+    { src = "git@github.com:folke/snacks.nvim" },
+    { src = "git@github.com:folke/which-key.nvim" },
+    { src = "git@github.com:folke/trouble.nvim" },
+    { src = "git@github.com:folke/todo-comments.nvim" },
+    { src = "git@github.com:akinsho/bufferline.nvim" },
+    -- Colourschemes
+    { src = "git@github.com:eddyekofo94/gruvbox-flat.nvim" },
+    { src = "git@github.com:ribru17/bamboo.nvim" },
+    -- Treesitter
+    { src = "git@github.com:nvim-treesitter/nvim-treesitter.git" },
+    { src = "git@github.com:nvim-treesitter/nvim-treesitter-textobjects.git" },
+    { src = "git@github.com:nvim-treesitter/nvim-treesitter-context.git" },
+    -- Code
+    { src = "git@github.com:lewis6991/gitsigns.nvim.git" },
+    -- Editing
+    { src = "git@github.com:nvim-mini/mini.ai" },
+    { src = "git@github.com:nvim-mini/mini.surround" },
 
 })
 
