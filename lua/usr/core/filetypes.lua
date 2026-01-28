1d02c28b576d10de012556d450bd647fb5ce825b
tslater <toby@tgs.onl>
2026-01-28 18:59:06 +0000

    feat(filetypes): Autocmd that sets filetype options

--- lua/usr/core/filetypes.lua
+++ lua/usr/core/filetypes.lua
@@ -3,3 +3,17 @@
 local filetype_configs = {
 
 }
+
+-- Create autocmds
+for _, config in ipairs(filetype_configs) do
+    vim.api.nvim_create_autocmd("FileType", {
+        pattern = config.pattern,
+        callback = function()
+            -- Set buffer-local options
+            if config.opts then
+                for k, v in pairs(config.opts) do
+                    vim.opt_local[k] = v
+                end
+            end
+    })
+end
