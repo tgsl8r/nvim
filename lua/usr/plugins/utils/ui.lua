11209731bd5a1d97d23525ac07849c8861add019
tslater <toby@tgs.onl>
2026-02-01 21:03:46 +0000

    feat(statusline): Add autocmds to set statusline

--- lua/usr/plugins/utils/ui.lua
+++ lua/usr/plugins/utils/ui.lua
@@ -76,4 +76,21 @@ end
 function Statusline.inactive()
 	return " %t"
 end
+
+local group = vim.api.nvim_create_augroup("StatusLine", { clear = true })
+
+vim.api.nvim_create_autocmd({ "WinEnter", "BufEnter" }, {
+	group = group,
+	desc = "Activate statusline on focus",
+	callback = function()
+		vim.opt_local.statusline = "%!v:lua.Statusline.active()"
+	end,
+})
+
+vim.api.nvim_create_autocmd({ "WinLeave", "BufLeave" }, {
+	group = group,
+	desc = "Deactivate statusline when unfocused",
+	callback = function()
+		vim.opt_local.statusline = "%!v:lua.Statusline.inactive()"
+	end,
 })
