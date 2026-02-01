a0aba238e766dc9cb21aa6e483f46ccecdf05e92
tslater <toby@tgs.onl>
2026-02-01 21:01:11 +0000

    feat(plugins/ui): Add git info statusline helper

--- lua/usr/plugins/utils/ui.lua
+++ lua/usr/plugins/utils/ui.lua
@@ -21,4 +21,26 @@ bufferline.setup({
 		},
 	},
 })
+
+-- status line helpers
+
+local function git()
+	local git_info = vim.b.gitsigns_status_dict
+	if not git_info or git_info.head == "" then
+		return ""
+	end
+
+	local head = git_info.head
+	local added = git_info.added and (" +" .. git_info.added) or ""
+	local changed = git_info.changed and (" ~" .. git_info.changed) or ""
+	local removed = git_info.removed and (" -" .. git_info.removed) or ""
+
+	return table.concat({
+		" ",
+		head,
+		added,
+		changed,
+		removed,
+	})
+end
 })
