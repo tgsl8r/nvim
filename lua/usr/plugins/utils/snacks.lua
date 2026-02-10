0e7a3bef1fe0fb45e11199ddcf48091f2a663a19
tslater <toby@tgs.onl>
2026-02-10 14:14:46 +0000

    style(snacks): Format

--- lua/usr/plugins/utils/snacks.lua
+++ lua/usr/plugins/utils/snacks.lua
@@ -1,33 +1,35 @@
 local snacks_status, snacks = pcall(require, "snacks")
 if not snacks_status then
-	print("Couldn't load snacks")
-	return
+    print("Couldn't load snacks")
+    return
 end
 
 snacks.setup({
-	bigfile = { enabled = false },
-	explorer = { enabled = true },
-	indent = {
+    bigfile = { enabled = false },
+    explorer = { enabled = true },
+    indent = {
         enabled = true,
         animate = {
             enabled = false
         },
     },
-	input = { enabled = true },
-	notifier = {
-		enabled = true,
-		timeout = 3000,
-	},
-	picker = { enabled = true },
-	quickfile = { enabled = true },
-	scope = { enabled = true },
-	scroll = { enabled = true },
-	statuscolumn = { enabled = true },
+    input = { enabled = true },
+    notifier = {
+        enabled = true,
+        timeout = 3000,
+    },
+    picker = {
+        enabled = true,
+    },
+    quickfile = { enabled = true },
+    scope = { enabled = true },
+    scroll = { enabled = true },
+    statuscolumn = { enabled = true },
     terminal = { enabled = true },
-	words = { enabled = true },
-	styles = {
-		notification = {
-			wo = { wrap = true } -- Wrap notifications
-		},
-	},
+    words = { enabled = true },
+    styles = {
+        notification = {
+            wo = { wrap = true } -- Wrap notifications
+        },
+    },
 })
