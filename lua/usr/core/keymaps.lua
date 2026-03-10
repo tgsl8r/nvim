67b6d3cf4ff31317108683956de4566fed61b533
tslater <toby@tgs.onl>
2026-03-10 16:44:06 +0000

    feat(keymaps): Add -P arg to snacks grep

--- lua/usr/core/keymaps.lua
+++ lua/usr/core/keymaps.lua
@@ -74,7 +74,7 @@ local bindings = {
     { "<S-l>",              "<cmd>bnext<cr>",                                                       desc = "Next Buffer" },
 
     -- search/show
-    { "<leader>sg",         function() Snacks.picker.grep() end,                                    desc = "Grep" },
+    { "<leader>sg",         function() Snacks.picker.grep({ args = { "-P" } }) end,                 desc = "Grep" },
     { "<leader>sw",         function() Snacks.picker.grep_word() end,                               desc = "Visual selection or word", mode = { "n", "x" } },
     { "<leader>sB",         function() Snacks.picker.grep_buffers() end,                            desc = "Grep Open Buffers" },
     { "<leader>sb",         function() Snacks.picker.lines() end,                                   desc = "Buffer Lines" },
