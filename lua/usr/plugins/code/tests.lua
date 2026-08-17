42674cfbb2d658c08ad60f776bd407d079369637
tslater <toby@tgs.onl>
2026-08-17 14:39:53 +0100

    feat(neotest): Add neotest_python config

--- lua/usr/plugins/code/tests.lua
+++ lua/usr/plugins/code/tests.lua
@@ -17,6 +17,7 @@ vim.pack.add({
 
 local neotest = require("neotest")
 local ctest = require("neotest-ctest")
+local neotest_python = require("neotest-python")
 
 neotest.setup({
 	adapters = {
@@ -24,5 +25,37 @@ neotest.setup({
 			build_dir = "build",
             ctest_command = "ctest --output-on-failure",
 		}),
+        neotest_python({
+            dap = { justMyCode = true },
+            args = { "-s", "-v", "--log-level", "DEBUG" },
+            env = {
+                TEST = "True",
+                TEST_MODE = "True",
+                DRYRUN = "True",
+                READONLY = "True",
+                SLACK_OUTPUT = "False",
+            },
+            runner = "pytest",
+            is_test_file = function(file_path)
+                return string.match(file_path, ".-/tests?/test[%w_]+%.py$")
+                    or string.match(file_path, ".-/testint/test[%w_]+%.py$")
+                    or string.match(file_path, ".-/test%-integration/test[%w_]+%.py$")
+            end,
+            pytest_discover_instances = false,
+        }),
 	},
+    summary = {
+        mappings = {
+            expand = "<CR>",
+            stop = "x",
+            run = "r",
+            run_marked = "R",
+            mark = "m",
+            clear_marked = "M",
+            output = "O",
+            short = "s",
+            attach = "a",
+            jumpto = "o",
+        },
+    },
 })
